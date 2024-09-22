10a5361106a535791512263a516184d81464a586
mestakevin <kmmesta@syr.edu>
2024-09-22 12:54:54 -0400

    edited organziation of main.py and ODE_methods, strated working on integration problem

--- Integral_methods.py
+++ Integral_methods.py
@@ -2,6 +2,17 @@
 
 #Simple Riemann sum
 
+def simpleRiemann(num_steps, domain, function):
+        area = 0 
+        step_size = domain / num_steps
+        pos = step_size / 2
+        while pos < domain: 
+            area += function(pos) * step_size
+            pos += step_size
+        return area
+
+
+
 #Trapezoidal Rule
 
 #Simpsons Rule
--- ODE_methods.py
+++ ODE_methods.py
@@ -1,12 +1,13 @@
 #ODE Integrators
+import matplotlib.pyplot as plt
+import math
 
-
-def floatInput(prompt):
-        num = float(input(prompt))
+def numInput(prompt):
+        try:
+            num = float(input(prompt))
+        except ValueError:
+            num = numInput("Invalid input, please enter a number: ")
         return num
-
-
-
 
 #Explict Method
 def explictMethod(mass, cur_x, cur_vel, force, dt):
@@ -17,3 +18,79 @@ def explictMethod(mass, cur_x, cur_vel, force, dt):
 #Fourth Order Runge-Kutta
 
 
+#calculates force
+def getForce(cur_x, cur_vel, k_cons, damp_cons):
+        force = (-k_cons * cur_x) - (damp_cons * cur_vel)
+        return force
+
+
+def oscillateSpring(mass, x0, v0, k_cons, damp_cons, dt):
+        pos_list = []
+        time_list = []
+        iteration = 0
+        time = 0
+        cur_x = x0
+        cur_vel = v0
+
+        while iteration < 800:
+            
+            pos_list.append(cur_x)
+            time_list.append(time)
+            
+            force = getForce(cur_x, cur_vel, k_cons, damp_cons)
+
+            cur_x, cur_vel = explictMethod(mass, cur_x, cur_vel, force, dt)
+        
+            time += dt
+            iteration += 1
+
+        return pos_list, time_list
+
+def analyticalSpring(mass, x0, v0, k_cons, damp_cons, dt): 
+        pos_list = []
+        time_list = []
+        iteration = 0
+        time = 0
+
+        cur_x = x0         
+        alp =  (-damp_cons / (2* mass))
+        #beta = ( ( (4 * mass * k_cons - (damp_cons ** 2) ) / (4* ( mass ** 2) ) ) ** 0.5 )
+        beta = ( (k_cons/mass) - (alp**2) ) ** 2 
+      
+        while iteration < 800:
+            pos_list.append(cur_x)
+            time_list.append(time)
+            
+            time += dt
+            cur_x = (math.exp(alp * time) )*(x0 * math.cos(beta * time ) + ((v0 - alp * x0) / beta) * math.sin(beta * time) )
+            iteration += 1        
+      
+        return pos_list, time_list
+
+def main():
+        mass = numInput("Please enter a mass: ")         
+        k_cons = numInput("Please enter a spring constant: ")
+        damp_cons  = numInput("Please enter a damping constant: ")
+
+        x_init = numInput("Please enter the initial position: ")
+        v_init = numInput("Please enter the initial velocity: ")
+        
+        dt = numInput("Please enter the time step: ")
+        
+        #pos_list, time_list = oscillateSpring(mass, x_init, v_init, k_cons, damp_cons, dt)
+        #pos_list, time_list = analyticalSpring(mass, x_init, v_init, k_cons, damp_cons, dt)
+
+        plt.figure()
+        
+        pos_list, time_list = oscillateSpring(mass, x_init, v_init, k_cons, damp_cons, dt)
+        plt.plot(time_list, pos_list, label = 'Explict')
+       
+        pos_list, time_list = analyticalSpring(mass, x_init, v_init, k_cons, damp_cons, dt)
+        plt.plot(time_list, pos_list, label = 'Analytical')
+        plt.legend()
+        plt.xlabel('Time')
+        plt.ylabel('X displacement')
+        plt.show()
+
+#run main
+main()
