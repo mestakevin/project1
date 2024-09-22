c579fa777897aa9cd97ccd4a8b40a656d57245da
mestakevin <kmmesta@syr.edu>
2024-09-22 15:51:29 -0400

    added trapezoid rule function for integration methods

--- Integral_methods.py
+++ Integral_methods.py
@@ -12,46 +12,66 @@ def numInput(prompt):
 
 
 def particleBoxProb(length, eng_lvl, pos):
-        return (2/length) * (math.sin( eng_lvl * math.pi * pos / length) ** 2)
+        return (2 / length) * ( (math.sin( (eng_lvl * math.pi * pos) / length)) ** 2)
 
-#Simple Riemann sum
 
-def plotParticleProb(num_steps, length, eng_lvl):
+def plotParticleProb(length, eng_lvl):
         Prob_list = []
         pos_list = []
         pos = 0 
         while pos <= length:
             pos_list.append(pos)
             Prob_list.append(particleBoxProb(length, eng_lvl, pos))
-            pos += length / num_steps
+            pos += length / 1000
 
         return pos_list, Prob_list
 
 
-def simpleRiemann(num_steps, domain, eng_lvl):
+def simpleRiemann(num_steps, length, eng_lvl, start, stop):
         area = 0 
-        step_size = domain / num_steps
-        pos = step_size / 2
-        while pos < domain: 
-            area += particleBoxProb(domain, eng_lvl, pos) * step_size
+        step_size = (stop - start) / num_steps
+        pos = start + step_size
+        while pos < stop: 
+            area += particleBoxProb(length, eng_lvl, pos) * step_size
             pos += step_size
+        return area
+
+def trapezoidRule(num_steps, length, eng_lvl, start, stop):
+        area = 0 
+        step_size = (stop - start) / num_steps
+        pos = start
+        i = 0 
+        while i <= num_steps:
+            if i == 0 or i == num_steps:
+                area += particleBoxProb(length, eng_lvl, pos)
+                i += 1
+                pos += step_size
+            else:
+                area += 2 * particleBoxProb(length, eng_lvl, pos)
+                i += 1
+                pos += step_size
+
+        area = (step_size / 2) * area
         return area
 
 def integrateProb():
         length = numInput("Please enter the length of the box: ")
         eng_lvl = numInput("Please enter the energy level: ")
         num_steps = numInput("Please enter the number of steps for integration: ")
+        start = numInput("Please enter from where you'd like to start the integral: ")
+        stop = numInput("Please enter where you;d like to end the integral: ")
 
         plt.figure()
         
-        pos_list, Prob_list = plotParticleProb(num_steps, length, eng_lvl)
+        pos_list, Prob_list = plotParticleProb(length, eng_lvl)
         plt.plot(pos_list, Prob_list)
         plt.xlabel('Particle Position')
         plt.ylabel('Probability Density')
         plt.show()
 
 
-        print("The calculated area using these inputs is:", simpleRiemann(num_steps, length, eng_lvl) ) 
+        print("The calculated area using Midpoint Riemann sum is:", simpleRiemann(num_steps, length, eng_lvl, start, stop) ) 
 
+        print("The calculated area using Trapezoid rule is:", trapezoidRule(num_steps, length, eng_lvl, start, stop) ) 
 
 integrateProb()
