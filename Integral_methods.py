4620101e45af4bd630fb09adee412d63d1084edf
mestakevin <kmmesta@syr.edu>
2024-09-25 00:00:35 -0400

    added in scipy methods for trapezoid and simpsons rule

--- Integral_methods.py
+++ Integral_methods.py
@@ -2,6 +2,7 @@
 
 import math
 import matplotlib.pyplot as plt
+from scipy import integrate as scint
 
 def numInput(prompt):
         try:
@@ -15,14 +16,15 @@ def particleBoxProb(length, eng_lvl, pos):
         return (2 / length) * ( (math.sin( (eng_lvl * math.pi * pos) / length)) ** 2)
 
 
-def plotParticleProb(length, eng_lvl):
+def plotParticleProb(length, eng_lvl, start, stop):
         Prob_list = []
         pos_list = []
-        pos = 0 
-        while pos <= length:
+        pos = start 
+        while pos <= stop:
             pos_list.append(pos)
             Prob_list.append(particleBoxProb(length, eng_lvl, pos))
-            pos += length / 1000
+            pos += length / 10000
+
 
         return pos_list, Prob_list
 
@@ -54,6 +56,38 @@ def trapezoidRule(num_steps, length, eng_lvl, start, stop):
         area = (step_size / 2) * area
         return area
 
+def scipyTrapezoid(num_steps, length, eng_lvl, start, stop):
+        step_size = (stop - start) / num_steps
+        pos_list, Prob_list = plotParticleProb(length, eng_lvl,start,stop)
+        return scint.trapezoid(Prob_list, pos_list, step_size)
+
+
+def simpsonsRule(num_steps, length, eng_lvl, start, stop):
+        area = 0 
+        step_size = (stop - start) / num_steps
+        pos = start
+        i = 0 
+        while i <= num_steps:
+            if i == 0 or i == num_steps:
+                area += particleBoxProb(length, eng_lvl, pos)
+                i += 1
+                pos += step_size
+            elif i%2 == 1:
+                area += 4 * particleBoxProb(length, eng_lvl, pos)
+                i += 1
+                pos += step_size
+            else:
+                area += 2* particleBoxProb(length, eng_lvl, pos)
+                i += 1
+                pos += step_size
+        area = (1/3) * step_size * area
+        return area
+
+def scipySimpson(num_steps, length, eng_lvl, start, stop):
+        step_size = (stop - start) / num_steps
+        pos_list, Prob_list = plotParticleProb(length, eng_lvl, start, stop)
+        return scint.simpson(Prob_list, pos_list, step_size)
+
 def compare():
         length = numInput("Please enter the length of the box: ")
         eng_lvl = numInput("Please enter the energy level: ")
@@ -63,7 +97,8 @@ def compare():
 
         plt.figure()
         
-        pos_list, Prob_list = plotParticleProb(length, eng_lvl)
+        pos_list, Prob_list = plotParticleProb(length, eng_lvl, 0,length)
+
         plt.plot(pos_list, Prob_list)
         plt.xlabel('Particle Position')
         plt.ylabel('Probability Density')
@@ -73,4 +108,9 @@ def compare():
         print("The calculated area using Midpoint Riemann sum is:", simpleRiemann(num_steps, length, eng_lvl, start, stop) ) 
 
         print("The calculated area using Trapezoid rule is:", trapezoidRule(num_steps, length, eng_lvl, start, stop) ) 
+            
+        print("The calculated area using Trapezoid rule from Scipy is:", scipyTrapezoid(num_steps, length, eng_lvl, start, stop) ) 
 
+        print("The calculated area using Simpsons rule is:", simpsonsRule(num_steps, length, eng_lvl, start, stop) ) 
+        
+        print("The calculated area using Simpsons rule from Scipy is:", scipySimpson(num_steps, length, eng_lvl, start, stop) ) 
